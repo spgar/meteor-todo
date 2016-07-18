3ca4c381ad7e7fe0dcfc2d91aab13106acd6c23f
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-07-18 09:08:44 -0400

    Adds simple tests.

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,33 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
+      });
+
+      it('can delete owned task', () => {
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        const invocation = { userId };
+        deleteTask.apply(invocation, [taskId]);
+        assert.equal(Tasks.find().count(), 0);
+      });
+    });
+  });
+}
